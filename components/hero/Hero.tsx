8d83c65fe86a9d6ada2032dd21ab0d1458b99a96
Nika Sepiskveradze <nika.sepiskveradze@gmail.com>
2023-10-03 21:22:25 +0400

    Make uppercase main hero title

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-center text-neutral-content">
         <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">War Track</h1>
+          <h1 className="mb-5 text-5xl font-bold uppercase">War Track</h1>
           <p className="mb-5">
             {`Elevate your awareness with real-time updates on the latest data
             surrounding the losses incurred in Ukraine's ongoing conflict.`}
